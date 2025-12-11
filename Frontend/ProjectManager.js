4ee058abf4f2a684a22c63c370921cbe8874f863
Alex. <alex.liska04@seznam.cz>
2025-12-11 18:55:07 +0100

    feat: add api endpoint for requesting users projects

--- Frontend/ProjectManager.js
+++ Frontend/ProjectManager.js
@@ -4,7 +4,7 @@ import User from './userState.js'
 export default class ProjectManager {
     constructor(events) {
         this.events = events;
-        this.AllProjects = [];
+        this.Projects = [];
         this.view = 'table';
 
         this.loadEvents();
@@ -12,17 +12,24 @@ export default class ProjectManager {
     }
     loadEvents() {
         this.events.on('request:user:projects', async () => {
-            const response = await fetch('http://localhost:5000/user/projects')
+            const response = await fetch('http://localhost:5000/user/projects',{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${User.token}`,
+                }
+            })
             if(response.ok) {
                 const data = await response.json();
-                console.log(data);
+                console.log('response',data);
+                this.Projects = data;
                 
             }
-            else {
+            /* else {
                 this.events.emit('user:retrieve:projects:failed', () => {
                     // Code to render message or whatever..
                 })
-            }
+            } */
         })
     }
     async loadUserProjects() {
